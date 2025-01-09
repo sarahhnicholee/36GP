d18ef044b9768d4a2c805e1b1d179111388e1b30
Sarah Parent <sarahparent@Sarahs-MacBook-Air.local>
2025-01-09 14:48:40 -0600

    finished

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,6 +1,44 @@
 const prisma = require("../prisma");
+const { faker } = require("@faker-js/faker");
+
 const seed = async () => {
   // TODO: Create 3 restaurants with 5 reservations each
+  for (let i = 0; i < 3; i++) {
+    const restaurant = await prisma.restaurant.create({
+      data: {
+        name: faker.company.name(),
+        reservations: {
+          create: [
+            {
+              name: faker.internet.username(),
+              email: faker.internet.email(),
+              partySize: 3,
+            },
+            {
+              name: faker.internet.username(),
+              email: faker.internet.email(),
+              partySize: 3,
+            },
+            {
+              name: faker.internet.username(),
+              email: faker.internet.email(),
+              partySize: 3,
+            },
+            {
+              name: faker.internet.username(),
+              email: faker.internet.email(),
+              partySize: 3,
+            },
+            {
+              name: faker.internet.username(),
+              email: faker.internet.email(),
+              partySize: 3,
+            },
+          ],
+        },
+      },
+    });
+  }
 };
 seed()
   .then(async () => await prisma.$disconnect())
